2564f2506f47ee187afd59a1a25917ef0e0e06ec
fzwirb <44208845+fzwirb@users.noreply.github.com>
2020-08-25 20:43:09 -0600

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,3 @@
+<?php 
+    $pdo = new PDO("pgsql:host=localhost;dbname=Github_Repo","postgres", "test");
+?>
--- github-repo.php
+++ github-repo.php
@@ -4,36 +4,31 @@
     $result = $pdo->query($query);
     $title = 'Below is our Github Repo';
 ?>
-<style>
-<?php 
-    include 'style.css';
-?>
-</style>
 <!doctype HTML>
 <head>
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 
 <body>
+
     <h1 class="title"><?php echo $title; ?></h1>
-    <?php 
-       foreach($result as $url) {
-    ?>
-        
-        <div class='container'>
-            <div class='row'>
-                <div class="col-md-4">
-                    <script src="http://gist-it.appspot.com/<?php echo $url['url']; ?>"></script>
-                </div>
-                <div class="col-md-4">
-                    <script src="http://gist-it.appspot.com/<?php echo $url['url']; ?>"></script>
-                </div>
-                <div class="col-md-4">
-                    <script src="http://gist-it.appspot.com/<?php echo $url['url']; ?>"></script>
-                </div>
+
+        <div class='container git-wrapper'>
+
+            <div class='row git-row'>
+
+                <?php foreach($result as $url) { ?>
+
+                    <div class="col-md-4 git-single">
+
+                        <script src="http://gist-it.appspot.com/<?php echo $url['url']; ?>"></script>
+
+                    </div>
+
+                <?php } ?>     
+
             </div>
-        </div>
-    <?php
-        }
-    ?>    
+
+        </div>  
+
 </body>
